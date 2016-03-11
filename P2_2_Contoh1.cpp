1fafd13be935c38860845491c619d0d3c4d488dc
sanhaji182 <sanhaji97@gmail.com>
2016-03-11 15:18:14 +0700

    Added files via upload

---
+++ P2_2_Contoh1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Halo JOMBLOOOOO!!!!" << endl;
+    return 0;
+}
---
+++ cin.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+int bil1;
+int bil2;
+
+cout <<"masukan bilangan 1 = ";
+cin >> bil1;
+cout <<"masukan bilangan 2 = ";
+cin >> bil2;
+cout << "Hasil Penjumlahan  = " << bil1+bil2 <<endl;
+cout << "Hasil Perkalian    = " << bil1*bil2<<endl;
+cout << "Hasil Pengurangan  = " << bil1-bil2 <<endl;
+cout << "Hasil Pembagian  = " << bil1/bil2;
+
+return 0;
+}
