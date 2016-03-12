8c3d94091586ec8089b862a8b1402536fd637aba
sanhaji182 <sanhaji97@gmail.com>
2016-03-12 16:44:57 +0700

    Update and rename P2_2_contoh4 (soal3) to P2_2_contoh4 (soal3).cpp

--- P2_2_contoh4 (soal3).cpp
+++ P2_2_contoh4 (soal3).cpp
@@ -3,15 +3,15 @@
 #include <iostream> 
 using namespace std; 
  
-void main() 
+int main() 
 { 
-  int nilai; 
+  int nilai; // integer untuk nilai
   nilai = 3>2; // hasil ungkapan : benar 
   cout << "nilai = "<<nilai<<endl; 
   nilai = 2>3; // hasil ungkapan : salah 
   cout << "nilai = " <<nilai<<endl; 
    
-  int x = 200; 
+  int x = 200; // mendefinisikan int x adalah 200
  
   cout<<"( x >= 1) && (x <= 50) -> " 
     <<(( x >= 1) && (x <= 50))<<'\n'; 
@@ -19,11 +19,11 @@ void main()
   cout<<"( x >= 1) || (x <= 50) -> " 
     <<(( x >= 1) || (x <= 50))<<'\n'; 
  
-  int bil1,bil2,minim; 
-  bil1 = 53; 
-  bil2 = 6; 
+  int bil1,bil2,minim; // variable bil1,bil2 dan minim adalah variable interger
+  bil1 = 53; //bil 1 adalah 53
+  bil2 = 6; //bil 2 adalah 6
    
   minim = bil1<bil2 ? bil1:bil2; 
   cout << "Bilangan terkecil =  "<<minim<<'\n'; 
- 
+ return 0;
 }
