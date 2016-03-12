9a3a38e6cdb26e1ad6dbc529621dbc156a878369
sanhaji182 <sanhaji97@gmail.com>
2016-03-12 16:57:39 +0700

    Added files via upload

---
+++ P2_2_Contoh3(Nama).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nama; // Variable string untuk nama
+    cout << "masukkan nama anda = ";
+    cin >> nama; // input nama yang akan ditampilan
+    cout << "Tampilan nama anda = " << nama; // output yang akan ditampilkan sesuai dengan nama yang di input
+    return 0;
+}
